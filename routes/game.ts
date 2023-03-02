ba7d5c8b41c93222006d0205e3e290acf01648d5
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2023-03-02 13:27:33 +0200

    -added logger to /games

--- routes/game.ts
+++ routes/game.ts
@@ -3,9 +3,11 @@ import { Request, Response } from "express";
 const games = express.Router()
 games.use(express.json())
 import {LOBBIES} from './queuing'
+import logger from "../utils/logger";
 
 //Get all lobbies
 games.get('/lobbylist', (req: Request, res: Response, err: Error) => {
+    logger.info('Get all lobbies');
     return res.json(LOBBIES)
 });
 
@@ -13,7 +15,11 @@ games.get('/lobbylist', (req: Request, res: Response, err: Error) => {
 games.get('/lobby/:id', (req: Request, res: Response, err: Error) => {
     const id = req.params.id
     const LOBBY = LOBBIES.find(lobby => lobby.lobbyId === parseInt(id))
-    if (LOBBY === undefined) return res.json({message: 'lobby not found'})
+    if(LOBBY === undefined) {
+        logger.error(`Lobby not found for id ${id}`);
+        return res.json({ message: 'lobby not found' });
+    }
+    logger.info(`Get data for a single lobby for id ${id}`);
     return res.json(LOBBY)
 });
 
@@ -21,9 +27,14 @@ games.get('/lobby/:id', (req: Request, res: Response, err: Error) => {
 games.get('/lobby/:id/:recentMove', (req: Request, res: Response, err: Error) => {
     const {id, recentMove} = req.params
     const LOBBY = LOBBIES.find(lobby => lobby.lobbyId === parseInt(id))
-    if(LOBBY === undefined) return res.json({message: 'lobby not found'})
-    else LOBBY.recentMove = recentMove
-    return res.json(LOBBY)
+    if(LOBBY === undefined) {
+        logger.error(`Lobby not found for id ${id}`);
+        return res.json({ message: 'lobby not found' });
+    } else {
+        LOBBY.recentMove = recentMove;
+        logger.info(`Moved 1 piece for lobby id ${id}`);
+        return res.json(LOBBY);
+      }
 });
 
 export default games
