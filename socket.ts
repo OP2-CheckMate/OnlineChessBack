118cafcf5bd4b9eb43e6c4362e89a082d5273049
samz9n <samuel.langhoff@outlook.com>
2023-04-03 16:32:49 +0300

    woeking on chatbox with socket

--- socket.ts
+++ socket.ts
@@ -1,38 +1,38 @@
-import { getLobbies, getLobby, movePiece } from "./socket/game";
-import { checkQueue, createlobby, joinlobby, joinqueue } from "./socket/queuing";
-import { Lobby, OneMove } from "./types/types";
+import { getLobbies, getLobby, movePiece } from './socket/game'
+import { checkQueue, createlobby, joinlobby, joinqueue } from './socket/queuing'
+import { Lobby, OneMove } from './types/types'
 
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.get('/', (req: Request, res: Response) => {
   return res.json()
-});
+})
 
-io.on('connection', (socket:any) => {
-  console.log(socket.rooms);
+io.on('connection', (socket: any) => {
+  console.log(socket.rooms)
 
-  socket.on('createLobby', (playerName:string) => {
+  socket.on('createLobby', (playerName: string) => {
     const result = createlobby(playerName, socket.id)
     io.to(socket.id).emit('createdLobby', result)
   })
 
-  socket.on('joinroom', (roomId:number) => {
+  socket.on('joinroom', (roomId: number) => {
     socket.join(roomId)
   })
-  
+
   socket.on('joinlobby', (lobbyId: number, name: string) => {
     const result = joinlobby(lobbyId, name, socket.id)
     const lobbyNotFound = 0
-    if (result.lobbyId === lobbyNotFound){
-    }else{
-        socket.join(result.lobbyId)
-        socket.to(result.lobbyId).emit('playerJoined', result)
-        io.to(socket.id).emit('createdLobby', result)
+    if (result.lobbyId === lobbyNotFound) {
+    } else {
+      socket.join(result.lobbyId)
+      socket.to(result.lobbyId).emit('playerJoined', result)
+      io.to(socket.id).emit('createdLobby', result)
     }
   })
 
@@ -50,19 +50,20 @@ io.on('connection', (socket:any) => {
     io.to(opponentId).emit('gameUpdate', game)
   })
 
-  socket.on('joinqueue', (playerName:string) => {
-    if (joinqueue(playerName, socket.id)){
-      const lobby: Lobby = checkQueue() || {lobbyId: 0}
-      if (lobby.lobbyId !== 0){
+  socket.on('joinqueue', (playerName: string) => {
+    if (joinqueue(playerName, socket.id)) {
+      const lobby: Lobby = checkQueue() || { lobbyId: 0 }
+      if (lobby.lobbyId !== 0) {
         io.to(lobby.player1?.id, lobby.player2?.id).emit('gamefound', lobby)
       }
     }
   })
 
-});
-
-
+  socket.on('chat-message', (msg: string) => {
+    socket.broadcast.emit('chat-message', msg)
+  })
+})
 
 server.listen(8080, () => {
-  console.log('listening on *:8080');
-});
+  console.log('listening on *:8080')
+})
